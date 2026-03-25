d00c115869de169f7769251373b387118a29410f
Ömer Acar <omeracar34@icloud.com>
2026-03-25 16:50:22 +0300

    postman collection added

--- src/main/java/com/omeracar/vault/controller/EmployeeController.java
+++ src/main/java/com/omeracar/vault/controller/EmployeeController.java
@@ -5,12 +5,19 @@ import com.omeracar.vault.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/employees")
 public class EmployeeController {
 
     @Autowired
     private EmployeeService employeeService;
+
+    @GetMapping()
+    public List<Employee> getAllEmployees() {
+        return employeeService.getAll();
+    }
 
     @GetMapping("/{id}")
     public Employee getById(@PathVariable Long id) {
